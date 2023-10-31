9680ce9128fa89329d4601beee3c39a299115dd6
Joao Vitor <joaovitormoraveis@gmail.com>
2023-10-30 20:01:05 -0400

    getting started with spring and springboot

---
+++ src/test/java/com/spring/action/tacocloud/controller/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package com.spring.action.tacocloud.controller;
+
+import com.spring.action.tacocloud.controllers.HomeController;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(Matchers.containsString("Welcome to...")));
+    }
+}
