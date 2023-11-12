4059fa2408e30d2ad47332962b1487f6ab93ed5c
Joao Vitor <joaovitormoraveis@gmail.com>
2023-11-12 17:43:35 -0500

    Fixing missing configuration annotation on web view config

--- src/main/java/com/spring/action/tacocloud/configuration/WebConfig.java
+++ src/main/java/com/spring/action/tacocloud/configuration/WebConfig.java
@@ -1,13 +1,15 @@
 package com.spring.action.tacocloud.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login");
+        registry.addViewController("/login").setViewName("login");
     }
 }
