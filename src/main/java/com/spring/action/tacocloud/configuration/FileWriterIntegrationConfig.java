6a3edbde63cb247640a2e16887eecaf424fb74de
Joao Vitor <joaovitormoraveis@gmail.com>
2023-11-15 19:11:53 -0500

    C10 configuring spring integration using Java

--- src/main/java/com/spring/action/tacocloud/configuration/FileWriterIntegrationConfig.java
+++ src/main/java/com/spring/action/tacocloud/configuration/FileWriterIntegrationConfig.java
@@ -1,11 +1,34 @@
 package com.spring.action.tacocloud.configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.transformer.GenericTransformer;
+
+import java.io.File;
 
 @Configuration
-@ImportResource("classpath:/filewriter-config.xml")
+//@ImportResource("classpath:/filewriter-config.xml")
 public class FileWriterIntegrationConfig {
+
+    @Bean
+    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return String::toUpperCase;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "textInChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(""));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+        return handler;
+    }
 }
 
