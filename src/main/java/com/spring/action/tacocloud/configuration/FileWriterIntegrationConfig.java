a15e94a2e2b8980f1b1ecad4c4b4736fb8856f78
Joao Vitor <joaovitormoraveis@gmail.com>
2023-11-15 19:18:04 -0500

    C10 configuring spring integration using Java

--- src/main/java/com/spring/action/tacocloud/configuration/FileWriterIntegrationConfig.java
+++ src/main/java/com/spring/action/tacocloud/configuration/FileWriterIntegrationConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.support.FileExistsMode;
 import org.springframework.integration.transformer.GenericTransformer;
+import org.springframework.messaging.MessageChannel;
 
 import java.io.File;
 
@@ -30,5 +32,17 @@ public class FileWriterIntegrationConfig {
         handler.setAppendNewLine(true);
         return handler;
     }
+
+    /* Message channels are created automatically, and it is optional to create the beans in this section
+
+    @Bean
+    public MessageChannel textInChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel fileWriterChannel() {
+        return new DirectChannel();
+    }*/
 }
 
